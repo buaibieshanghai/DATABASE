44b3086f6fe90ae9ce2a90a2ffe54f8418fe9554
孙笑川 <57168798+buaibieshanghai@users.noreply.github.com>
2020-12-09 16:11:28 +0800

    Update 建库.sql

--- 创建数据库/建库.sql
+++ 创建数据库/建库.sql
@@ -1,5 +1,8 @@
---如果数据库已经存在,则不创建,不存在则创建
-if not exists(select * from sys.databases where name = 'BookManage')
+--如果数据库已经存在,则删掉重新创建,不存在则创建
+if  exists(select * from sys.databases where name = 'BookManage')
+begin 
+drop database bookmanage
+end
 begin
 create database bookmanage
 on primary(
@@ -17,4 +20,3 @@ log on(
 	filegrowth= 10%
 ) 
 end
-            
